6dcdd52d0284b87d26cb8624fe4e1d0b483c2c55
Tao <hitchaitao@outlook.com>
2020-01-10 11:13:00 -0500

    1st leetcode346

---
+++ src/Leetcode346MovingAveragefromDataStream.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Leetcode346MovingAveragefromDataStream {
+    int sum;
+    Queue<Integer> s;
+    int size;
+    /** Initialize your data structure here. */
+    public Leetcode346MovingAveragefromDataStream(int size) {
+        this.sum = 0;
+        this.s = new LinkedList<>();
+        this.size = size;
+    }
+
+    public double next(int val) {
+        if(s.size()<size){
+            sum += val;
+            s.offer(val);
+            return (double) sum/s.size();
+        }else{
+            sum -=s.poll();
+            sum +=val;
+            s.offer(val);
+            return (double) sum/s.size();
+        }
+
+    }
+}
