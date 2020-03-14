f1d49748e0a343fb498955b882445da23d0b621d
Tao Desktop <hitchaitao@outlook.com>
2020-03-14 11:48:31 -0400

    1st 1110

---
+++ src/Leetcode1110DeleteNodesAndReturnForest.java
@@ -0,0 +1,21 @@
+public class Leetcode1110DeleteNodesAndReturnForest {
+    List<TreeNode> ans = new ArrayList<>();
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        HashSet<Integer> del = new HashSet<>();
+        for(int i : to_delete) del.add(i);
+        root = helper(del,root);
+        if(root!=null) ans.add(root);
+        return ans;
+    }
+    private TreeNode helper(Set<Integer> del,TreeNode node){
+        if(node==null) return null;
+        node.right = helper(del,node.right);
+        node.left = helper(del,node.left);
+        if(!del.contains(node.val)) return node;
+        if(node.left!=null) ans.add(node.left);
+        if(node.right!=null) ans.add(node.right);
+        return null;
+    }
+}
+// 时间：0（n）
+// 空间：O（h）
