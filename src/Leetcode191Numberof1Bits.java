0013928ae9c718ea4230f7db076fc1690341c7e0
Taochai <42437872+Taochai@users.noreply.github.com>
2020-04-18 16:55:12 -0400

    Leetcode191 1st

---
+++ src/Leetcode191Numberof1Bits.java
@@ -0,0 +1,11 @@
+public class Leetcode191Numberof1Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n!=0){
+            count++;
+            n = n&(n-1); // 消除n里最右的一个1
+        }
+        return count;
+    }
+}
