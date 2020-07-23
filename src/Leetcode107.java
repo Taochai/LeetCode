564bb44943770786bc6f587d9f1635d7346160b5
Eric <qinshuichaitao@gmail.com>
2020-07-23 16:48:51 -0400

    commit

--- src/Leetcode107.java
+++ src/Leetcode107.java
@@ -1,39 +1,39 @@
-public class Leetcode107 {
-
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
-    class Solution {
-        List<List<Integer>> ans = new ArrayList<>();
-        public List<List<Integer>> levelOrderBottom(TreeNode root) {
-            if(root==null) return ans;
-            Queue<TreeNode> q = new LinkedList<>();
-            q.add(root);
-            while(!q.isEmpty()){
-                int size = q.size();
-                List<Integer> tmpL = new ArrayList<>();
-                for(int i=0;i<size;i++){
-                    TreeNode node = q.poll();
-                    tmpL.add(node.val);
-                    if(node.left!=null) q.add(node.left);                                                         if(node.right!=null) q.add(node.right);
-                }
-                ans.add(tmpL);
-            }
-            Collections.reverse(ans);
-            return ans;
-        }
-
-    }
-}
+//public class Leetcode107 {
+//
+//    /**
+//     * Definition for a binary tree node.
+//     * public class TreeNode {
+//     *     int val;
+//     *     TreeNode left;
+//     *     TreeNode right;
+//     *     TreeNode() {}
+//     *     TreeNode(int val) { this.val = val; }
+//     *     TreeNode(int val, TreeNode left, TreeNode right) {
+//     *         this.val = val;
+//     *         this.left = left;
+//     *         this.right = right;
+//     *     }
+//     * }
+//     */
+//    class Solution {
+//        List<List<Integer>> ans = new ArrayList<>();
+//        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//            if(root==null) return ans;
+//            Queue<TreeNode> q = new LinkedList<>();
+//            q.add(root);
+//            while(!q.isEmpty()){
+//                int size = q.size();
+//                List<Integer> tmpL = new ArrayList<>();
+//                for(int i=0;i<size;i++){
+//                    TreeNode node = q.poll();
+//                    tmpL.add(node.val);
+//                    if(node.left!=null) q.add(node.left);                                                         if(node.right!=null) q.add(node.right);
+//                }
+//                ans.add(tmpL);
+//            }
+//            Collections.reverse(ans);
+//            return ans;
+//        }
+//
+//    }
+//}
--- src/test.java
+++ src/test.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
+    static int b;
     public static void main(String[] args) {
-        System.out.println(555);
+
+        System.out.println(16>>2);
     }
     //test````````````````
     //test 笔记本
+
+
+
 }
