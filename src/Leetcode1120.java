1ead7ee07b49a23b342bb1445449efd695e15f6f
Eric <qinshuichaitao@gmail.com>
2020-09-06 08:31:30 -0400

    leetcode

---
+++ src/Leetcode1120.java
@@ -0,0 +1,18 @@
+public class Leetcode1120 {
+    double ans = Double.MIN_VALUE;
+    public double maximumAverageSubtree(TreeNode root) {
+        helper(root);
+        return ans;
+    }
+
+    private double[] helper(TreeNode root){
+        if(root==null){
+            return new double[]{0, 0.0}; //number，sum
+        }
+        double[] left = helper(root.left);
+        double[] right = helper(root.right);
+        double avg = (left[1]+right[1]+root.val)/(1+left[0]+right[0]);
+        ans = Math.max(avg,ans);
+        return new double[]{1+left[0]+right[0],left[1]+right[1]+root.val};
+    }
+}
---
+++ src/Leetcode187.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Leetcode187 {
+    /*
+    * 存在hashset中，十个十个找，重复就加在答案里
+    * */
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> set = new HashSet<>();
+        Set<String> set1 = new HashSet<>();
+
+        for(int i=0;i<s.length()-9;i++){
+            String ss = s.substring(i,i+10);
+            if(set.contains(ss)){
+                set1.add(ss);
+            }else{
+                set.add(ss);
+            }
+        }
+        return new ArrayList<>(set1);
+    }
+}
