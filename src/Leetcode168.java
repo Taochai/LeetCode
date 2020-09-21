e30b0d5c40216cf13875d87d1d4fbed01f08ecde
Eric <qinshuichaitao@gmail.com>
2020-09-21 01:43:05 -0400

    leetcode

---
+++ src/Leetcode168.java
@@ -0,0 +1,19 @@
+public class Leetcode168 {
+
+    public String convertToTitle(int n) {
+        String ans = "";
+        while(n > 0){
+            if(n%26 != 0){
+                ans = String.valueOf((char)(n%26 + 64)) + ans;
+                n /= 26;
+            }else{
+                ans = "Z" + ans;
+                // n--;
+                n /= 26;
+                n--;
+                //上一位如果是Z， 前面就会少1.
+            }
+        }
+        return ans;
+    }
+}
