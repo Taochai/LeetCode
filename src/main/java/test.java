e4f780d65d24abf60e409db5a1a99f48b51141c5
Eric <qinshuichaitao@gmail.com>
2020-11-07 22:25:46 -0500

    leetcode

---
+++ src/main/java/test.java
@@ -0,0 +1,16 @@
+import leetcode.Leetcode718;
+
+public class test {
+
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1,2,3,2,1};
+        int[] arr2 = new int[]{3,2,1,4,7};
+        Leetcode718 test = new Leetcode718();
+
+    }
+
+}
+
+
+
