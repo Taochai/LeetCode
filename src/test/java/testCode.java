4de66bedc189718e3c914cb6c656d777e9e87a11
Eric <qinshuichaitao@gmail.com>
2021-01-09 00:38:49 +0800

    test

---
+++ src/test/java/testCode.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class testCode {
+
+    @Test
+    public void testNumber(){
+        int a = 10^9;
+        int b = 1000000000;
+        int c = (int)Math.pow(10,9);
+        System.out.println(a==b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(b==c);
+    }
+}
