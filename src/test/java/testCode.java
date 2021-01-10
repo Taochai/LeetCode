a9974dc4409a81e5564c7f606c105d8f84b56641
Eric <qinshuichaitao@gmail.com>
2021-01-10 11:30:24 +0800

    Eric: modify testCode file

--- src/test/java/testCode.java
+++ src/test/java/testCode.java
@@ -8,8 +8,6 @@ public class testCode {
         int b = 1000000000;
         int c = (int)Math.pow(10,9);
         System.out.println(a==b);
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(b==c);
+
     }
 }
