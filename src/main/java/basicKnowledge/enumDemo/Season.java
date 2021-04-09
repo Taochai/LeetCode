f3af62584869faa76fc065579330fe7b6f8dbfa6
tao.chai <tao.chai@thoughtworks.com>
2021-04-09 14:44:18 +0800

    enum demo code: implements interface

--- src/main/java/basicKnowledge/enumDemo/Season.java
+++ src/main/java/basicKnowledge/enumDemo/Season.java
@@ -3,6 +3,7 @@ package basicKnowledge.enumDemo;
 public enum Season {
     /*
      * 定义了的对象。
+     * 如果enum类implements了接口，可以在enum类里实现，也可以在每一个enum对象里实现。
      * */
     SPRING("春天"),
     SUMMER("夏天");
