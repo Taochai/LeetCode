9853851afb5757d7e60a1c00da06678c707e0edd
tao.chai <tao.chai@thoughtworks.com>
2021-04-21 10:39:13 +0800

    Leetcode24 2more solution

--- src/main/java/leetcode/Leetcode24.java
+++ src/main/java/leetcode/Leetcode24.java
@@ -29,6 +29,32 @@ public class Leetcode24 {
         node.next.next = node;
         return node.next;
     }
+
+//    public ListNode swapPairs(ListNode head) {
+//        ListNode pre = new ListNode(0);
+//        pre.next = head;
+//        ListNode temp = pre;
+//        while(temp.next != null && temp.next.next != null) {
+//            ListNode start = temp.next;
+//            ListNode end = temp.next.next;
+//            temp.next = end;
+//            start.next = end.next;
+//            end.next = start;
+//            temp = start;
+//        }
+//        return pre.next;
+//    }
+//
+
+    //递归
+//    public ListNode swapPairs(ListNode head) {
+//        if(head == null || head.next == null) return head;
+//        ListNode next = head.next;
+//        head.next = swapPairs(next.next);
+//        next.next = head;
+//        return next;
+//    }
+
 }
 
 
