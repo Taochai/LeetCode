20ee96d1d200f8f31e4c2da0656b6e6c0e321496
Tao Chai <chaitao1994@outlook.com>
2021-05-06 23:30:51 +0800

    leetcode177 sql

---
+++ src/main/java/leetcode/Leetcode177.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class Leetcode177 {
+
+//    CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+//    BEGIN
+//    set N:=N-1;
+//    RETURN (
+//      # Write your MySQL query statement below.
+//            select Salary
+//            from Employee
+//            group by Salary
+//            order by Salary desc
+//            limit N,1
+//    );
+//    END
+//CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+//    BEGIN
+//    set N:=N-1;
+//    RETURN (
+//      # Write your MySQL query statement below.
+//            select distinct Salary
+//            from Employee
+//            order by Salary desc
+//            limit N,1
+//    );
+//    END
+}
