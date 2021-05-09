991642f17b5196cc24ef6db078600fd8a304cd61
Tao Chai <chaitao1994@outlook.com>
2021-05-10 00:40:20 +0800

    comment leetcode55

--- src/main/java/leetcode/Leetcode55JumpGame.java
+++ src/main/java/leetcode/Leetcode55JumpGame.java
@@ -6,8 +6,8 @@ public class Leetcode55JumpGame {
 
         if(nums.length==1) return true;
         int i=0;
-        int maxIdx = 0;
-        while(i<=maxIdx){
+        int maxIdx = 0; //max index can be reached
+        while (i <= maxIdx) { //i 最多走到maxIdx，每往前一步，检查maxIdx是否到end.
             maxIdx = Math.max(maxIdx,i+nums[i]);
             if(maxIdx>=nums.length-1) return true;
             i++;
